a82e9df7e648822ae7335dac65648d19a6317a39
Emmanuel Onyekachi <emmanuelonyekachi04122000@gmail.com>
2025-05-15 11:19:04 +0100

    Created USer profile view

--- account/views.py
+++ account/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from account.serializers import AccountSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+@api_view(['get', 'patch'])
+@permission_classes([IsAuthenticated])
+def profile(request):
+    user = request.user
+    
+    if request.method == 'GET':
+        serializer = AccountSerializer(user)
+        return Response(serializer.data)
+    
+    elif request.method == 'PATCH':
+        serializer = AccountSerializer(user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
